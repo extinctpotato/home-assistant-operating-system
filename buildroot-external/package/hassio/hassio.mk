25cae66019f75dd452de936cd3aea65e7763f2b3
Adam Olech <me@aole.ch>
2024-08-07 23:23:32 +0200

    package/hassio: download static distro

--- buildroot-external/package/hassio/hassio.mk
+++ buildroot-external/package/hassio/hassio.mk
@@ -4,33 +4,21 @@
 #
 ################################################################################
 
-HASSIO_VERSION = 1.0.0
+HASSIO_VERSION = v4.0.0
 HASSIO_LICENSE = Apache License 2.0
 # HASSIO_LICENSE_FILES = $(BR2_EXTERNAL_HASSOS_PATH)/../LICENSE
-HASSIO_SITE = $(BR2_EXTERNAL_HASSOS_PATH)/package/hassio
-HASSIO_SITE_METHOD = local
-HASSIO_VERSION_URL = "https://version.home-assistant.io/stable.json"
-
-HASSIO_CONTAINER_IMAGES_ARCH = supervisor dns audio cli multicast observer core
-
-define HASSIO_CONFIGURE_CMDS
-	# Deploy only landing page for "core" by setting version to "landingpage"
-	curl -s $(HASSIO_VERSION_URL) | jq '.core = "landingpage"' > $(@D)/stable.json
-endef
-
-define HASSIO_BUILD_CMDS
-	$(Q)mkdir -p $(@D)/images
-	$(Q)mkdir -p $(HASSIO_DL_DIR)
-	$(foreach image,$(HASSIO_CONTAINER_IMAGES_ARCH),\
-		$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/fetch-container-image.sh \
-			$(BR2_PACKAGE_HASSIO_ARCH) $(BR2_PACKAGE_HASSIO_MACHINE) $(@D)/stable.json $(image) "$(HASSIO_DL_DIR)" "$(@D)/images"
-	)
-endef
+HASSIO_SOURCE = $(patsubst "%",%,$(BR2_PACKAGE_HASSIO_ARCH))-$(patsubst "%",%,$(BR2_PACKAGE_HASSIO_MACHINE))-distro.img.xz 
+HASSIO_SITE = https://github.com/extinctpotato/hassio-container-store-builder/releases/download/$(HASSIO_VERSION)
+HASSIO_DEPENDENCIES = host-xz
 
 HASSIO_INSTALL_IMAGES = YES
 
 define HASSIO_INSTALL_IMAGES_CMDS
-	$(BR2_EXTERNAL_HASSOS_PATH)/package/hassio/create-data-partition.sh "$(@D)" "$(BINARIES_DIR)"
+	$(INSTALL) -D -m 0644 $(@D)/data.ext4 $(BINARIES_DIR)/data.ext4
+endef
+
+define HASSIO_EXTRACT_CMDS
+	xz -c -d $(HASSIO_DL_DIR)/$(HASSIO_SOURCE) > $(@D)/data.ext4
 endef
 
 $(eval $(generic-package))
